3f18bfe5fd5e037a32cc0147c4f6981f995c0850
alwolfeCBE <83241214+alwolfeCBE@users.noreply.github.com>
2024-09-03 16:37:19 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+        int exit = 1;
+        int [] arr = new int[100];
+        while (exit != 0) {
+            displayM();
+        }
+    }
+   //display method to show user menu
+   public static void displayM(){
+
+   }
+   //method to sequentially populate array
+    public static int[] popSeq(int[] arr){
+
+        return arr;
+    }
+
+
+
+
+}
